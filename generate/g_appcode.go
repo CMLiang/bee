41b0f828441e75d1d7f43cade0b488a3c5159859
CMLiang <751957195@qq.com>
2019-05-14 15:30:28 +0800

    .....

--- generate/g_appcode.go
+++ generate/g_appcode.go
@@ -11,7 +11,7 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations
 // under the License......
-//first push
+//@CMLiang edit 2019-05-14 15:25
 
 package generate
 
