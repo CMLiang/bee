c5f57eb790806c1ef65f26b84c926821c4120ecf
CMLiang <751957195@qq.com>
2019-05-15 16:20:32 +0800

    生成orm结构

--- generate/g_appcode.go
+++ generate/g_appcode.go
@@ -42,6 +42,7 @@ type DbTransformer interface {
 	GetTableNames(conn *sql.DB) []string
 	GetConstraints(conn *sql.DB, table *Table, blackList map[string]bool)
 	GetColumns(conn *sql.DB, table *Table, blackList map[string]bool)
+	// GetRelationColumns(table *Table, tr *TableRelation, getDirection int8)
 	GetGoDataType(sqlType string) (string, error)
 }
 
@@ -150,6 +151,7 @@ type Table struct {
 }
 
 var trlist []TableRelation
+var correcttrlist []TableRelation
 
 // TableRelation represent a relationships between tables
 type TableRelation struct {
@@ -375,6 +377,29 @@ func getTableObjects(tableNames []string, db *sql.DB, dbTransformer DbTransforme
 	for _, tb := range tables {
 		dbTransformer.GetColumns(db, tb, blackList)
 	}
+
+	// fmt.Println(trlist)
+	for _, tb := range tables {
+		//先处理逆向的关系，如找到RelationName，进入处理
+		//TableRelation中的isCorrect会被置为True
+		for _, tr := range trlist {
+			var correcttr = tr
+			// correcttr.IsCorrect = true
+			if tb.Name == tr.RelationName {
+				GetRelationColumns(tb, tr, -1)
+				correcttr.IsCorrect = true
+			}
+			correcttrlist = append(correcttrlist, correcttr)
+		}
+	}
+	for _, tb := range tables {
+		//再处理正向的关系，进入处理
+		for _, tr := range correcttrlist {
+			if tb.Name == tr.SourceName && tr.IsCorrect == true {
+				GetRelationColumns(tb, tr, 1)
+			}
+		}
+	}
 	return
 }
 
@@ -427,12 +452,14 @@ func (*MysqlDB) GetConstraints(db *sql.DB, table *Table, blackList map[string]bo
 // GetColumns retrieves columns details from
 // information_schema and fill in the Column struct
 func (mysqlDB *MysqlDB) GetColumns(db *sql.DB, table *Table, blackList map[string]bool) {
-	if strings.Contains(table.Name, "_has_") {
+	//表间多对多关系约定为中间表（MySQL model约定）
+	if strings.Contains(table.Name, "_has_") && len(table.Name) > 5 {
 		trm2m := new(TableRelation)
 		trm2m.SourceName = table.Name[0:strings.LastIndex(table.Name, "_has_")]
-		trm2m.RelationName = table.Name[strings.LastIndex(table.Name, "_has_"):len(table.Name)]
+		trm2m.RelationName = table.Name[strings.LastIndex(table.Name, "_has_")+5 : len(table.Name)]
 		trm2m.RelM2M = true
 		trm2m.ReverseMany = true
+		// fmt.Println(*trm2m)
 		trlist = append(trlist, *trm2m)
 	}
 
@@ -453,16 +480,15 @@ func (mysqlDB *MysqlDB) GetColumns(db *sql.DB, table *Table, blackList map[strin
 	for colDefRows.Next() {
 		var trFlag bool = false
 		//The initial table relationship
-		tr := TableRelation{
-			SourceName:   table.Name,
-			RelationName: "inexistence",
-			RelOne:       false,
-			ReverseOne:   false,
-			RelO2M:       false,
-			ReverseMany:  false,
-			RelM2M:       false,
-			IsCorrect:    true,
-		}
+		tr := new(TableRelation)
+		tr.SourceName = table.Name
+		tr.RelationName = "inexistence"
+		tr.RelOne = false
+		tr.ReverseOne = false
+		tr.RelO2M = false
+		tr.ReverseMany = false
+		tr.RelM2M = false
+		tr.IsCorrect = false
 		// datatype as bytes so that SQL <null> values can be retrieved
 		var colNameBytes, dataTypeBytes, columnTypeBytes, isNullableBytes, columnDefaultBytes, extraBytes, columnCommentBytes []byte
 		if err := colDefRows.Scan(&colNameBytes, &dataTypeBytes, &columnTypeBytes, &isNullableBytes, &columnDefaultBytes, &extraBytes, &columnCommentBytes); err != nil {
@@ -550,14 +576,14 @@ func (mysqlDB *MysqlDB) GetColumns(db *sql.DB, table *Table, blackList map[strin
 			//表中含_id结尾的字段，约定_id前的编码为表编码
 			//如表profile中有user_id字段，则RelationName的值取user
 			//后续会校验是否存在对应表
-			if strings.HasSuffix(col.Name, "_id") {
-				tr.RelationName = col.Name[0:strings.LastIndex(col.Name, "_id")]
+			if strings.HasSuffix(colName, "_id") {
+				tr.RelationName = colName[0:strings.LastIndex(colName, "_id")]
 				tr.RelOne = true
 				tr.ReverseOne = true
 				trFlag = true
-			} else if strings.HasSuffix(col.Name, "_ids") {
+			} else if strings.HasSuffix(colName, "_ids") {
 				//同上，判断_ids结尾的为从表。
-				tr.RelationName = col.Name[0:strings.LastIndex(col.Name, "_ids")]
+				tr.RelationName = colName[0:strings.LastIndex(colName, "_ids")]
 				tr.RelO2M = true
 				tr.ReverseMany = true
 				trFlag = true
@@ -566,11 +592,66 @@ func (mysqlDB *MysqlDB) GetColumns(db *sql.DB, table *Table, blackList map[strin
 		col.Tag = tag
 		table.Columns = append(table.Columns, col)
 		if trFlag {
-			fmt.Println(tr)
-			trlist = append(trlist, tr)
-			fmt.Println(trlist)
+			// fmt.Println(tr)
+			trlist = append(trlist, *tr)
 		}
 	}
+}
+
+// GetRelationColumns retrieves the columns details of relationship between tables
+// from information_schema and fill in the Column struct
+func GetRelationColumns(table *Table, tr TableRelation, getDirection int8) {
+	// create a column
+	rcol := new(Column)
+
+	// Tag info
+	tag := new(OrmTag)
+	// tag.Column = tr.RelationName
+	tag.Auto = false
+	tag.AutoNow = false
+	tag.AutoNowAdd = false
+	tag.Pk = false
+	tag.Null = true
+	tag.Unique = false
+	if getDirection == 1 {
+		//如果为正向，则取RelationName为字段名
+		rcol.Name = utils.CamelCase(tr.RelationName)
+		if tr.RelOne {
+			rcol.Type = "*" + utils.CamelCase(tr.RelationName)
+			tag.Comment = "设置与" + tr.RelationName + "一对一关系，该表为扩展表，字段：" + tr.RelationName + "_id为关联字段。"
+			tag.RelOne = true
+		} else if tr.RelO2M {
+			rcol.Type = "*" + utils.CamelCase(tr.RelationName)
+			tag.Comment = "设置与" + tr.RelationName + "一对多关系，该表为从表，字段：" + tr.RelationName + "_ids为关联字段。"
+			tag.RelFk = true
+		} else if tr.RelM2M {
+			rcol.Name = rcol.Name + "s"
+			rcol.Type = "[]*" + utils.CamelCase(tr.RelationName)
+			tag.Comment = "设置与" + tr.RelationName + "多对多关系，中间表：" + tr.SourceName + "_has_" + tr.RelationName
+			tag.RelM2M = true
+		}
+	} else if getDirection == -1 {
+		rcol.Name = utils.CamelCase(tr.SourceName)
+		if tr.ReverseOne {
+			rcol.Type = "*" + utils.CamelCase(tr.SourceName)
+			tag.Comment = "设置与" + tr.SourceName + "一对一关系，该表为主表，字段：id为关联字段。"
+			tag.ReverseOne = true
+		} else if tr.ReverseMany {
+			rcol.Name = rcol.Name + "s"
+			rcol.Type = "[]*" + utils.CamelCase(tr.SourceName)
+			tag.Comment = "设置与" + tr.SourceName + "一对多关系，该表为主表，字段：id为关联字段。"
+			tag.ReverseMany = true
+		} else if tr.ReverseMany {
+			rcol.Name = rcol.Name + "s"
+			rcol.Type = "[]*" + utils.CamelCase(tr.SourceName)
+			tag.Comment = "设置与" + tr.SourceName + "多对多关系，中间表：" + tr.SourceName + "_has_" + tr.RelationName
+			tag.ReverseMany = true
+		}
+	}
+	// fmt.Println(tr)
+	// fmt.Println(tag.Comment)
+	rcol.Tag = tag
+	table.Columns = append(table.Columns, rcol)
 }
 
 // GetGoDataType maps an SQL data type to Golang data type
